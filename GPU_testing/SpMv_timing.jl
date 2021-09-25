c8108575107e43da155a0e77b1b26269f55de507
Toby Harvey <tharvey@cs.uoregon.edu>
2021-09-24 17:39:18 -0700

    looks like gpu will be faster

--- GPU_testing/SpMv_timing.jl
+++ GPU_testing/SpMv_timing.jl
@@ -2,12 +2,13 @@ include("../numerical.jl")
 include("../physical_params.jl")
 include("../domain.jl")
 
-using CUDA
+using CUDA.CUSPARSE
 using Printf
 using LinearAlgebra
+using SparseArrays
 
 let 
-
+    
     p = 2 
     
     Lw = 1
@@ -25,7 +26,7 @@ let
     (y1, y2, y3, y4) = (0, 0, Lw, Lw)
     xt, yt = transfinite(x1, x2, x3, x4, y1, y2, y3, y4)
 
-    nes = 8 * 2 .^ (1:5)
+    nes = 8 * 2 .^ (4:8)
 
     for ne in nes
         
@@ -43,13 +44,16 @@ let
         Λ = dynamicblock(block_ops)
         u = rand(size(Λ)[2])
 
-        @time for _ in 1:10000
+        @time for _ in 1:100
             mul!(u, Λ, u, 1, 0)
         end
         
-        
+        dΛ = CuSparseMatrixCSR(Λ)
+        du = CuArray(u)
+
+        @time for _ in 1:100
+            CUSPARSE.mv!('N', 1, dΛ, du, 0, du, 'a')
+        end
+
     end
-
-
-
 end
